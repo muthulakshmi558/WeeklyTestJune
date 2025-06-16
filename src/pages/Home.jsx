93d9375a3d7d9cc58ecbf4f5df84c577861ead6d
Muthulakshmi <muthulakshmi5293@gmail.com>
2025-06-16 14:08:59 +0530

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,15 +3,10 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className="container text-center mt-5">
-      <h1 className="display-4">🍽️ Recipe Sharing App</h1>
+      <h1 className="display-4"> Recipe Sharing App</h1>
       <p className="lead">Cook it. Share it. Love it.</p>
 
-      <img
-        src="https://img.freepik.com/premium-photo/top-view-delicious-chicken-meal-arrangement_23-2148763876.jpg"
-        alt="Delicious food"
-        className="img-fluid rounded shadow mb-4"
-        style={{ maxHeight: '300px' }}
-      />
+      
 
       <div className="d-flex justify-content-center gap-3">
         <Link to="/recipes" className="btn btn-primary btn-lg">📋 View Recipes</Link>
